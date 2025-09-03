4c8e57bcad7185574dce37c53b9771a099124847
tiniaAa <franciscovolovik@gmail.com>
2025-09-03 03:22:04 -0300

    incompleto (En edicion)

--- Activdad3.3/Ejercicio1/Models/Estancia.cs
+++ Activdad3.3/Ejercicio1/Models/Estancia.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,48 @@ namespace Ejercicio1.Models
         public string Nombre { get; set; }
         public Casco Casco { get; set; }
 
-        public Estancia(string nombre, string idCampo, double sup )
+        ArrayList puestos = new ArrayList();
+
+        ArrayList Campos = new ArrayList();
+        public Estancia(string nombre, string idCampo, double superficie  )
         {
 
+            this.Nombre = nombre;
+            this.Casco=new Casco();
+            this.Casco.Administrador = "Sin asignar ";
+            Puesto puesto = new Puesto();
+            puestos.Add( puesto);
+            puesto.Responsable = "Sin asignar ";
+
+
+            Campo campo = new Campo(idCampo,superficie);
+            Campos.Add(campo);
+
+
         }
+        #region administrar campos 
+
+
+        public Campo AgregarCampos(string identificador, double superficie )
+        {
+            Campo nuevo = new Campo(identificador, superficie);
+            Campos.Add(nuevo);
+            return nuevo;
+        }
+        public Campo VerCampo (int idx)
+        {
+            if (Campos!=null && idx>=0 && idx < Campos.Count)
+            {
+                return Campos[idx] as Campo;
+            }
+            return null;
+        }
+        public int VerCantidadDeCampos()
+        {
+            return Campos.Count;
+        }
+
+        #endregion
+
     }
 }
